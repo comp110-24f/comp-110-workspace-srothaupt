c199c9934cf2361d3ebd286480301669b775ce5c
Simone Rothaupt <rothaupt@unc.edu>
2024-08-20 17:27:53 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in COMP 110!"""
+
+__author__ = "730673654"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="Campers")
